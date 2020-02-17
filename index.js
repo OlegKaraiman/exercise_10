42448f5d02c30918af1c1f1550d21895c9f34cf8
OlegKaraiman <59221843+OlegKaraiman@users.noreply.github.com>
2020-02-17 13:50:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function myPadString(str, lengthStr, symbol, flag) {
-    let resStr = '';
-    let resSymbol = '';
-    let str1 = str;
-    const symbol1 = symbol;
-    if (str1.length >= lengthStr) {
-        for (let i = 0; i < lengthStr; i++) {
-            resStr += str1[i];
-        }
-    } else if (str1.length < lengthStr) {
-        for (let i = str1.length; i < lengthStr; i++) {
-            if (flag === 'r' || !flag) {
-                resStr = str1 += symbol1;
-            } else if (flag === 'l') {
-                resSymbol += symbol1;
-                resStr = resSymbol + str1;
+ function myPadString(str, lengthStr, symbol, flag) {
+            let resStr = '';
+            let resSymbol = '';
+            let str1 = str;
+            const symbol1 = symbol;
+            if (str1.length >= lengthStr) {
+                for (let i = 0; i < lengthStr; i++) {
+                    resStr += str1[i];
+                }
+            } else if (str1.length < lengthStr) {
+                for (let i = str1.length; i < lengthStr; i++) {
+                    if (!flag) { 
+                        resStr = str1 += symbol1;
+                    } else if (flag) {
+                        resSymbol += symbol1;
+                        resStr = resSymbol + str1;
+                    }
+                }
             }
+            return resStr;
         }
-    }
-    return resStr;
-}
-myPadString('string', '18', '!', 'l');
+        myPadString('string', '1', '!');
